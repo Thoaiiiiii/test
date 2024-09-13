81481f367638056427062ec4dab0042b59695aaf
Quoc Thoai <109143761+Thoaiiiiii@users.noreply.github.com>
2024-09-13 21:31:50 +0700

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,24 @@
-print('hello world')
-print('pham quoc thoai')
+# print('hello world')
+# print('pham quoc thoai')
+
+# list in python
+    # 1 len, type
+    # 2 insert(index, '')
+    # 3 append('')
+    # 4 extend : noi 2 chuoi
+    # 5 remove
+    # 6 pop(index) : ko index thi revome last item
+    # 7 del mylist[index] : ko index remove all list
+    # 8 clear : remove all list
+    # 9 loop list
+mylist = ['apple', 'banana', 'kiwi']
+# print(mylist)
+# print(len(mylist))
+# print(type(mylist))
+# mylist.insert(2, 'watermelone')
+# mylist.append('orange')
+mylist2 = ['mango', 'papaya']
+# mylist.extend(mylist2)
+# mylist.remove('banana')
+# mylist.pop(1)
+# del mylist[0]
